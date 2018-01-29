280ddfb86112e7df08716096f73afe57cd202ad7
Jonathan Dahan <hi@jonathan.is>
2018-01-29 16:07:21 -0500

    Huge rewrite - we now have basic socket.io support

--- readme.md
+++ readme.md
@@ -1,5 +1,39 @@
-room-http server
+# room-server
 
-    POST /assert {fact}
-    POST /retract {fact}
-    POST /select {facts} => [facts...]
+Creates a room database that you can connect to over HTTP and socket.io
+
+You can test it out by running `node server` after installing the dependencies with `yarn` or `npm install`
+
+For motivations, context, and philosophy, check out https://github.com/jedahan/research
+
+## example http
+
+    $ curl -d '{"fact": "#curl is an app at (20, 30)"}' -H "Content-Type: application/json" localhost:3000/assert
+    {"id":1}
+
+    $ curl -d '{"facts": ["$what is an app at ($x, $y)"]}' -H "Content-Type: application/json" localhost:3000/select
+    {"solutions":[{"what":{"name":"curl"},"x":20,"y":30}]}
+
+## example websocket
+
+from [examples/browser.js](examples/browser.js)
+
+```javascript
+const socket = io.connect(`http://localhost:3000`)
+
+socket.on('solutions', solutions => {
+  solutions.forEach(solution => {
+    console.log(`what am i? a ${solution.what.str}`)
+  })
+})
+
+socket.on('id', ({id}) => {
+  console.log(`got id '${id}'`)
+})
+
+socket.emit('assert', ['i am a browser'])
+
+setInterval(() => {
+  socket.emit('select', ['i am a $what'])
+}, 5000)
+```
--- server.js
+++ server.js
@@ -1,86 +1,13 @@
 const PORT = process.env.PORT || 3000
 
-const RoomDB = require('roomdb')
+const room = require('./lib/room.js')
 
-const Koa = require('koa')
-const koaStatic = require('koa-static')
-const route = require('koa-route')
-const koaBody = require('koa-body')
-const koaCors = require('koa-cors')
-const util = require('util')
-
-const room = new RoomDB()
-const clients = new Map()
-
-const app = new Koa()
-app.use(koaCors({origin: true}))
-app.use(koaBody())
-
-app.use(async (ctx, next) => {
-  if (ctx.method !== 'POST') return next()
-  const requestBody = util.inspect(ctx.request.body)
-  console.log(`<- ${ctx.url} ${requestBody}`)
-  await next()
-  const responseBody = util.inspect(ctx.body)
-  console.log(`-> ${ctx.url} ${responseBody}`)
-})
-
-// On the first POST, allow asserting a client id maybe
-app.use(async (ctx, next) => {
-  if (ctx.method !== 'POST') return next()
-
-  const id = ctx.request.body.id
-
-  ctx.state.client = clients.get(id) || room.connect(id)
-
-  if (!clients.has(ctx.state.client.id)) {
-    clients.set(ctx.state.client.id, ctx.state.client)
-  }
-
-  await next()
-})
-
-const assert = async (ctx, next) => {
-  const {fact} = ctx.request.body
-  await ctx.state.client.assert(fact)
-  ctx.body = {id: ctx.state.client.id}
-}
-
-const retract = async (ctx, next) => {
-  const {fact} = ctx.request.body
-  await ctx.state.client.retract(fact)
-  ctx.body = 'okay!'
-}
-
-const facts = async (ctx, next) => {
-  ctx.body = room.facts
-}
-
-const select = async (ctx, next) => {
-  const {facts} = ctx.request.body
-  await ctx.state.client
-    .select(facts)
-    .doAll(solutions => { ctx.body = { solutions } })
-}
-
-const disconnect = async (ctx, next) => {
-  await ctx.state.client
-    .disconnect()
-    .do(obj => { ctx.body = obj })
-}
-
-app.use(route.post('/assert', assert))
-app.use(route.post('/select', select))
-app.use(route.post('/retract', retract))
-app.use(route.post('/disconnect', disconnect))
-
-app.use(route.post('/facts', facts))
-
-app.use(koaStatic('./'))
+let app = require('./lib/httpServer.js')(room)
+    app = require('./lib/socketServer.js')(room, app)
 
 app.listen(PORT)
 
-console.log(`listening on ${PORT}`)
+console.log(`http and websocket server listening on :${PORT}`)
 
 process.on('SIGINT', () => {
   console.log()
