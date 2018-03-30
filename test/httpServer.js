30fe2b8111e85951a9c93b8dafd8142795bcdd99
Jonathan Dahan <hi@jonathan.is>
2018-03-30 18:08:01 -0400

    fix ava tests

--- test/httpServer.js
+++ test/httpServer.js
@@ -10,15 +10,15 @@ const createApp = () => httpServer((new Database().client('http'))).listen()
 
 test('assert adds to the log', async t => {
   const app = createApp()
-  await request(app).post('/assert').send({ fact: gorogInitial }).expect(200)
+  await request(app).post('/assert').send({ facts: gorogInitial }).expect(200)
   const facts = await request(app).post('/facts')
   t.deepEqual(facts.body, {solutions: [ gorogInitial ]})
 })
 
 test('retract removes from the log', async t => {
   const app = createApp()
-  await request(app).post('/assert').send({ fact: gorogInitial }).expect(200)
-  await request(app).post('/retract').send({ fact: '#gorog is a barbarian at $x, $y' }).expect(200)
+  await request(app).post('/assert').send({ facts: gorogInitial }).expect(200)
+  await request(app).post('/retract').send({ facts: '#gorog is a barbarian at $x, $y' }).expect(200)
   const facts = await request(app).post('/facts')
   t.deepEqual(facts.body, {solutions: [ ]})
 })
@@ -27,14 +27,14 @@ test.todo('retracts and asserts batch correctly')
 
 test('select grabs the right fact', async t => {
   const app = createApp()
-  await request(app).post('/assert').send({ fact: gorogInitial }).expect(200)
-  await request(app).post('/retract').send({ fact: gorogInitial }).expect(200)
-  await request(app).post('/assert').send({ fact: gorogMoves }).expect(200)
+  await request(app).post('/assert').send({ facts: gorogInitial }).expect(200)
+  await request(app).post('/retract').send({ facts: gorogInitial }).expect(200)
+  await request(app).post('/assert').send({ facts: gorogMoves }).expect(200)
 
   const facts = await request(app).post('/facts')
   t.deepEqual(facts.body, {solutions: [ gorogMoves ]})
 
-  const res = await request(app).post('/select').send({ fact: ['$name is a $what at $x, $y'] })
+  const res = await request(app).post('/select').send({ facts: ['$name is a $what at $x, $y'] })
   const [{name, what, x, y}] = res.body.solutions
   t.is(name.id, 'gorog')
   t.is(what.word, 'barbarian')
