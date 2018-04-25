6f80665c4c9ed7b43f1794a3579e1db07c003468
Jonathan Dahan <hi@jonathan.is>
2018-04-25 11:07:00 -0400

    Initial implementation of mdns for service discovery

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const chalk = require('chalk')
 const boxen = require('boxen')
-const ip = require('ip')
-const PORT = process.env.PORT || 3000
+const chalk = require('chalk').default
+const mdns = require('mdns')
+const PORT = parseInt(process.env.PORT || '3000')
+const OSC_PORT = parseInt(process.env.OSC_PORT || '41234')
 
 const Database = require('@living-room/database-js')
 const room = new Database()
@@ -13,36 +14,59 @@ app = require('./lib/socketServer.js')(app, { verbose: false })(
 
 let osc = require('./lib/oscServer.js')(room.client('osc'))
 
-app.listen(PORT)
+const services = new Map([
+  [mdns.tcp('http', 'livingroom').toString(), new Map()],
+  [mdns.udp('osc', 'livingroom').toString(), new Map()],
+  [mdns.tcp('socketio', 'livingroom').toString(), new Map()]
+])
 
-osc.open()
+console.dir(services)
 
-let message = `
-  ${chalk.green('Serving a living room!')}
+const draw = () => {
+  let message = ['living room servers at', '']
+  for (const [, types] of services) {
+    for (const [id, {up}] of types) {
+      message.push(up ? chalk.green(id) : chalk.red(id))
+    }
+  }
 
-${chalk.bold('locally')}
-http://localhost:${PORT}
-socketio://localhost:${PORT}
-osc://localhost:41234
-`
-
-try {
-  const ipAddress = ip.address()
-  message += `
-${chalk.bold('on the network')}
-http://${ipAddress}:${PORT}
-socketio://${ipAddress}:${PORT}
-osc://${ipAddress}:41234
-`
-} catch (err) {}
-
-console.log(
-  boxen(message, {
+  const formatting = {
+    borderColor: 'white',
     padding: 1,
-    borderColor: 'green',
     margin: 1
+  }
+  console.log(boxen(message.join('\n'), formatting))
+}
+
+for (let service of services.keys()) {
+  const browser = mdns.createBrowser(mdns.makeServiceType(service))
+
+  browser.on('serviceUp', wut => {
+    console.dir(wut)
+    const {host, port, addresses, type} = wut
+    console.log(type.toString())
+    const matching = services.get(type.toString())
+    console.dir(matching)
+    const id = `${type.name}://${host}:${port}`
+    matching.set(id, {host, port, addresses, type, up: true})
+    draw()
   })
-)
+
+  browser.on('serviceDown', ({type, name, replyDomain}) => {
+    const types = services.get(type.toString())
+    const id = `${type.name}://${name}.${replyDomain}`
+    for (const seen of types.keys()) {
+      if (seen.startsWith(id)) {
+        types.get(seen).up = false
+      }
+    }
+    draw()
+  })
+  browser.start()
+}
+
+osc.listen(OSC_PORT)
+app.listen(PORT)
 
 process.on('SIGINT', () => {
   console.log()
